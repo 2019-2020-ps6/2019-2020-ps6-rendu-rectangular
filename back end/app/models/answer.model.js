b077808853632fa7017f851b2eec152040327164
Yann Mouchon <yannmouchon@gmail.com>
2020-03-24 08:39:22 +0100

    Changement du backend

--- back end/app/models/answer.model.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi')
-const BaseModel = require('../utils/base-model.js')
-
-module.exports = new BaseModel('Question', {
-    type: Joi.string(), 
-    value: Joi.string().required(), 
-    idCorrect: Joi.boolean().required(),
-    questionId: Joi.number(),
-})
