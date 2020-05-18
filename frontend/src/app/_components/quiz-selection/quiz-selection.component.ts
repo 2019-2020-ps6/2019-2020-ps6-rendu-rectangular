1e76be6e7f9f7d0c0aaac47cf45ce0d76039cbe1
YannMouchon <yannmouchon@gmail.com>
2020-05-18 17:53:49 +0200

    Un patient ne peut plus créer de quiz

--- frontend/src/app/_components/quiz-selection/quiz-selection.component.ts
+++ frontend/src/app/_components/quiz-selection/quiz-selection.component.ts
@@ -5,6 +5,7 @@ import { PlayService } from 'src/services/play.service';
 import { Router } from '@angular/router';
 import { UserService } from 'src/services/user.service';
 import { UtilService } from 'src/services/util.service';
+import { User } from 'src/models/user.model';
 
 @Component({
   selector: 'app-quiz-selection',
@@ -14,14 +15,17 @@ import { UtilService } from 'src/services/util.service';
 export class QuizSelectionComponent implements OnInit {
 
   public quizList: Quiz[] = [];
+  user: User;
 
   constructor(private quizService: QuizService,
               private playService: PlayService,
               private router: Router,
-              private utilService: UtilService) {
+              private utilService: UtilService,
+              private userService: UserService) {
     this.quizService.quizzes$.subscribe((quizzes: Quiz[]) => {
       this.quizList = quizzes;
     });
+    this.userService.currentUser$.subscribe((user: User) => this.user = user)
   }
 
   ngOnInit() {
