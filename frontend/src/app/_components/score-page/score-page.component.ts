f658f4d094c17af53e2f4091152909355c891018
YannMouchon <yannmouchon@gmail.com>
2020-05-20 15:01:41 +0200

    Possibilté de choisir thème et niveau dans les scores

--- frontend/src/app/_components/score-page/score-page.component.ts
+++ frontend/src/app/_components/score-page/score-page.component.ts
@@ -4,6 +4,8 @@ import { PlayService } from 'src/services/play.service';
 import { UserService } from 'src/services/user.service';
 import { QuizGame } from 'src/models/gameQuiz.model';
 import { UtilService } from 'src/services/util.service';
+import { QuizService } from 'src/services/quiz.service';
+import { Theme } from 'src/models/theme.model';
 
 @Component({
   selector: 'app-score-page',
@@ -15,10 +17,14 @@ export class ScorePageComponent implements OnInit {
   user: User;
   playersQuizGame: QuizGame[];
   scores: Array<Array<number>>;
+  niveau: string = "all";
+  themesStr: string[];
+  themeSelected: string = "all";
 
   constructor(private playService: PlayService,
               private userService: UserService,
-              private utilService: UtilService
+              private utilService: UtilService,
+              private quizService: QuizService
     ) {
     this.userService.updateUser();
     this.userService.currentUser$.subscribe((user: User) => {
@@ -29,6 +35,10 @@ export class ScorePageComponent implements OnInit {
         .reverse();
         this.scores = this.playersQuizGame.map((quizgame: QuizGame) => this.playService.calculateScoreOfGame(quizgame));
       });
+    });
+    this.quizService.setThemesFromUrl();
+    this.quizService.themes$.subscribe((themes: Theme[]) => {
+      this.themesStr = themes.map((theme: Theme) => theme.theme);
     });
   }
 
